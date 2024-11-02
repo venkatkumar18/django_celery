cf208745cf1c18c10f0fc39d8a221f3149721db5
Venkat Kumar <isvenkatkumar@gmail.com>
2024-11-02 20:14:31 +0530

    tech(dcelery): Version 1 setup - docker,redis,celery,django

---
+++ commands.md
@@ -0,0 +1,3 @@
+1) startproject - django-admin startproject dcelery
+2) pip freeze > requirements.txt
+3) docker-compose up -d --build
---
+++ dcelery/dcelery/celery.py
@@ -0,0 +1,12 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcelery.settings')
+
+app = Celery('dcelery')
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
+
+@app.task
+def add_number():
+    return sum(range(100))
